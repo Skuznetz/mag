46b025dd24e91b1e578663eff7383b0cc99d0e3b
Skuznetz <s.kuznetzov@mail.ru>
2017-08-16 19:26:56 +0300

    пишем миграцию

--- db/migrate/20170816161613_create_items.rb
+++ db/migrate/20170816161613_create_items.rb
@@ -1,6 +1,10 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
+      t.string :name
+      t.text  :description
+      t.float :price
+      t.float :weight
 
       t.timestamps
     end
