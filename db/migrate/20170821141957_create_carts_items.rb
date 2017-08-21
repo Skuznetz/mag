3182f6f3a1c1bd1d6b691b5feb78dea5cbae08f2
Skuznetz <s.kuznetzov@mail.ru>
2017-08-21 17:31:56 +0300

    Напишем миграцию

--- db/migrate/20170821141957_create_carts_items.rb
+++ db/migrate/20170821141957_create_carts_items.rb
@@ -1,6 +1,14 @@
 class CreateCartsItems < ActiveRecord::Migration[5.0]
-  def change
-    create_table :carts_items do |t|
-    end
+  def self.up
+    create_table :carts_items, id: false do |t|
+      t.references :cart
+      t.references :item
+  end
+  add_index :carts_items, [:item_id, :cart_id]
+  add_index :carts_items, [:cart_id, :item_id]
+end
+  def self.down
+    drop_table :carts_items
   end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817163000) do
+ActiveRecord::Schema.define(version: 20170821141957) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "carts_items", id: false, force: :cascade do |t|
+    t.integer "cart_id"
+    t.integer "item_id"
+    t.index ["cart_id", "item_id"], name: "index_carts_items_on_cart_id_and_item_id"
+    t.index ["cart_id"], name: "index_carts_items_on_cart_id"
+    t.index ["item_id", "cart_id"], name: "index_carts_items_on_item_id_and_cart_id"
+    t.index ["item_id"], name: "index_carts_items_on_item_id"
   end
 
   create_table "items", force: :cascade do |t|
