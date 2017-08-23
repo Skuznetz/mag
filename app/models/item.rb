eae0c7c6be83464e0f54c4eb54202b3d0245a527
Skuznetz <s.kuznetzov@mail.ru>
2017-08-23 18:10:39 +0300

    ассоциация модели item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   has_and_belongs_to_many :carts
   validates :price, :weight, numericality: { greater_than: 0 },
-presence: true
+  presence: true
+  has_many :positions
+  has_many :carts, through: :positions
 end
