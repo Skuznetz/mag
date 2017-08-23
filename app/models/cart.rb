5a93f15c34cea9af166ba234edf756824431a303
Skuznetz <s.kuznetzov@mail.ru>
2017-08-23 18:09:54 +0300

    ассоциация модели cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,4 +3,6 @@ class Cart < ApplicationRecord
   validates  :user, presence: true
   validates  :user_id, uniqueness: true
   has_and_belongs_to_many :items
+  has_many :positions
+  has_many :items, through: :positions
 end
