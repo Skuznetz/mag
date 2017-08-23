f2809a4517b0dce8fad0ac165d08d2c7e313f54e
Skuznetz <s.kuznetzov@mail.ru>
2017-08-23 18:16:41 +0300

    метод добавления товара

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,4 +5,12 @@ class Cart < ApplicationRecord
   has_and_belongs_to_many :items
   has_many :positions
   has_many :items, through: :positions
+
+  def add_item(i)
+    if items.include?(i)
+      positions.where(item_id: i.id).first.increment!(:quantity)
+    else
+      positions.create(item: i, quantity: 1)
+    end
+  end
 end
