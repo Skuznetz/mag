5196e04bb67729107c2875a4236656fa4de93e4c
Skuznetz <s.kuznetzov@mail.ru>
2017-08-27 17:37:13 +0300

    форматируем item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,10 @@
 class Item < ApplicationRecord
   has_and_belongs_to_many :carts
-  validates :price, :weight, numericality: { greater_than: 0 },
-  presence: true
+  validates :price, :weight, numericality: { greater_than: 0 },presence: true
   has_many :positions
   has_many :carts, through: :positions
   has_many :images, as: :imageable
   has_many :positions
-  has_many :carts, through: :positions, source: :container,
-  source_type: "Cart"
-  has_many :orders, through: :positions, source: :container,
-  source_type: "Order"
+  has_many :carts, through: :positions, source: :container,source_type: "Cart"
+  has_many :orders, through: :positions, source: :container,source_type: "Order"
 end
