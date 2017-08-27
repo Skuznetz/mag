fc456ec61079f65785f8c91831e5989390c1782c
Skuznetz <s.kuznetzov@mail.ru>
2017-08-27 17:37:46 +0300

    форматируем position.rb

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,7 +1,6 @@
 class Position < ApplicationRecord
   belongs_to :item
   belongs_to :cart
-  attr_accessible :container, :container_id, :item, :item_id,
-:quantity
-belongs_to :container, polymorphic: true
+  attr_accessible :container, :container_id, :item, :item_id,:quantity
+  belongs_to :container, polymorphic: true
 end
