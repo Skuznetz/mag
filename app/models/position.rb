54b37cdb55bb4856e6384592bb5112baec769138
Skuznetz <s.kuznetzov@mail.ru>
2017-08-27 17:34:25 +0300

    добавим связь модели position

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,7 @@
 class Position < ApplicationRecord
   belongs_to :item
   belongs_to :cart
+  attr_accessible :container, :container_id, :item, :item_id,
+:quantity
+belongs_to :container, polymorphic: true
 end
