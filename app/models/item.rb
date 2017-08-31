19967ff679ccfefb8fc4f5414732b12783c7dc8f
Skuznetz <s.kuznetzov@mail.ru>
2017-08-31 08:25:41 +0300

    добавим поле в CATEGORY

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,13 @@ class Item < ApplicationRecord
   has_many  :positions
   has_many  :carts, through: :positions, source: :container,source_type: "Cart"
   has_many  :orders, through: :positions, source: :container,source_type: "Order"
+
+  after_destroy :decrement_category_counter
+private
+def increment_category_counter
+category.inc!(:items_count)
+end
+def decrement_category_counter
+category.inc!(:items_count, -1)
+end
 end
